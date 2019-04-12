abdd9450c2be137b6b6e878ee065a71879366311
Peter Kavounas <PKavounas@eastsideprep.org>
2019-04-12 15:02:14 -0700

    First chat server build

---
+++ chatPK/src/main/java/com/mycompany/chatpk/ChatServer.java
@@ -0,0 +1,13 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.chatpk;
+import static spark.Spark.*;
+
+public class ChatServer {
+    public static void main(String[] args) {
+        get("/hello", (req, res) -> "Hello World");
+    }
+}
